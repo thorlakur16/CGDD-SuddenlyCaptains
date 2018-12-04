c7ee70da97291c008a9d6ffa1f2e57d21af7461b
Hordur <hordurk16@ru.is>
2018-12-04 16:47:53 +0000

    Left right, menu updated

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+public class MainMenu : MonoBehaviour {
+    public EventSystem es;
+    private GameObject storeSelected;
+    // Use this for initialization
+    void Start()
+    {
+        storeSelected = es.firstSelectedGameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (es.currentSelectedGameObject != storeSelected)
+        {
+            if (es.currentSelectedGameObject == null)
+            {
+                es.SetSelectedGameObject(storeSelected);
+            }
+            else
+            {
+                storeSelected = es.currentSelectedGameObject;
+            }
+        }
+
+    }
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+
+}
