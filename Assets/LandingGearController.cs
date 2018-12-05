a20df444bb10593495e3c7d02d815277b9461186
Þorlákur Lyngmo <thorlakur16@ru.is>
2018-12-05 18:13:44 +0000

    nothing works

---
+++ Assets/LandingGearController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LandingGearController : MonoBehaviour {
+
+    public Animator animator;
+    private bool open;
+	// Use this for initialization
+	void Start () {
+        open = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void LandingGearDown()
+    {
+        animator.SetTrigger("openGear");
+        animator.ResetTrigger("closeGear");
+        open = true;
+    }
+
+    public void LandingGearUp()
+    {
+        animator.SetTrigger("closeGear");
+        animator.ResetTrigger("openGear");
+        open = false;
+    }
+
+    public bool isOpen()
+    {
+        return open;
+    }
+}
---
+++ Assets/TerminalLandingGearController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerminalLandingGearController : MonoBehaviour {
+
+    public LandingGearController landingGear1;
+    //public LandingGearController landingGear2;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButton("Fire1_P1"))
+        {
+            if (landingGear1.isOpen())
+            {
+                landingGear1.LandingGearUp();
+            }
+            else
+            {
+                landingGear1.LandingGearDown();
+            }
+        }
+        if (Input.GetButton("Fire1_P2"))
+        {
+            if (landingGear1.isOpen())
+            {
+                landingGear1.LandingGearUp();
+            }
+            else
+            {
+                landingGear1.LandingGearDown();
+            }
+        }
+    }
+}
