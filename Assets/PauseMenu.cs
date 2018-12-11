0be76b4a70d18930f25916092a64ae95f0d79e4e
Hordur <hordurk16@ru.is>
2018-12-11 16:38:17 +0000

    pause menu, restart button, altitude meter

---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour {
+
+    public static bool GameIsPaused;
+    public GameObject pauseMenuUI;
+	
+	// Update is called once per frame
+	void Update () {
+        //Debug.Log("startsadasdasd");
+        if (Input.GetButtonDown("Start_P1") || Input.GetButtonDown("Start_P1"))
+        {
+            Debug.Log("start");
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+
+    }
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    } 
+    public void QuitToMainMenu()
+    {
+        SceneManager.LoadScene(0);
+        Resume();
+    }
+    
+}
