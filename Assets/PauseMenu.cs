3ff025fc843a6a57003dba90c116c3e4109612e1
Hordur <hordurk16@ru.is>
2018-12-12 17:54:48 +0000

    Menu shi

--- Assets/PauseMenu.cs
+++ Assets/PauseMenu.cs
@@ -2,25 +2,55 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
 
 public class PauseMenu : MonoBehaviour {
 
     public static bool GameIsPaused;
     public GameObject pauseMenuUI;
-	
-	// Update is called once per frame
-	void Update () {
+    public EventSystem es;
+    private GameObject storeSelected;
+
+    void Awake()
+    {
+        storeSelected = es.firstSelectedGameObject;
+        GameIsPaused = true;
+        //Time.timeScale = 0f;
+        //Debug.Log(storeSelected);
+    }
+    // Update is called once per frame
+    void Update () {
         //Debug.Log("startsadasdasd");
-        if (Input.GetButtonDown("Start_P1") || Input.GetButtonDown("Start_P1"))
-        {
-            Debug.Log("start");
-            if (GameIsPaused)
+        //Debug.Log(es.firstSelectedGameObject);
+
+            if (Input.GetButtonDown("Start_P1") || Input.GetButtonDown("Start_P2"))
             {
-                Resume();
+                //Debug.Log("start");
+                if (GameIsPaused)
+                {
+                    Resume();
+                }
+                else
+                {
+                    Pause();
+                }
             }
-            else
+        if (GameIsPaused)
+        {
+            //Debug.Log("viddifag");
+            
+            if (es.currentSelectedGameObject != storeSelected)
             {
-                Pause();
+                Debug.Log(es.currentSelectedGameObject);
+                Debug.Log(storeSelected);
+                if (es.currentSelectedGameObject == null)
+                {
+                    es.SetSelectedGameObject(storeSelected);
+                }
+                else
+                {
+                    storeSelected = es.currentSelectedGameObject;
+                }
             }
         }
 
